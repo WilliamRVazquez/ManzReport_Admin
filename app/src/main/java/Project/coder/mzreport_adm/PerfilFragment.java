38e11034e1e5221884cbc258ce4e040465404037
Calixto01 <ccalixto@ucol.mx>
2022-11-18 18:43:13 -0600

    cambios p

--- app/src/main/java/Project/coder/mzreport_adm/PerfilFragment.java
+++ app/src/main/java/Project/coder/mzreport_adm/PerfilFragment.java
@@ -1,13 +1,14 @@
 package Project.coder.mzreport_adm;
 
-import android.content.DialogInterface;
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
@@ -16,7 +17,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -31,18 +31,21 @@ import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 
-import java.util.concurrent.Executor;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.annotation.Nullable;
+
 
 public class PerfilFragment extends Fragment {
     TextView fullName, email, phone;
     FirebaseAuth fAuth;
     FirebaseFirestore fStore;
-    String userId;
-    Button resetPassLocal, changeProfileImage, buttonsalir;
+    String userId,newPassword;
+    Button resetPassLocal, changeProfileImage, buttonsalir,sireset,noreset;
     FirebaseUser user;
-    ImageView profileImage;
+    Dialog dialog;
+    EditText Nombre,Email,Phone, resetPassword;
 
     @RequiresApi(api = Build.VERSION_CODES.P)
     @Override
@@ -50,43 +53,30 @@ public class PerfilFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View root = inflater.inflate(R.layout.fragment_perfil, container, false);
-
+        dialog =new Dialog(this.getContext());
+        resetPassword = new EditText(this.getContext());
+        sireset = new Button(this.getContext());
+        noreset = new Button(this.getContext());
         phone = root.findViewById(R.id.profilePhone);
         fullName = root.findViewById(R.id.profileName);
         email = root.findViewById(R.id.profileEmail);
         resetPassLocal = root.findViewById(R.id.resetPasswordLocal);
         buttonsalir = root.findViewById(R.id.button);
-
-        profileImage = root.findViewById(R.id.profileImage);
         changeProfileImage = root.findViewById(R.id.changeProfile);
-
-
         fAuth = FirebaseAuth.getInstance();
         fStore = FirebaseFirestore.getInstance();
-
         userId = fAuth.getCurrentUser().getUid();
         user = fAuth.getCurrentUser();
-        buttonsalir.setOnClickListener(view -> {
-            AlertDialog.Builder alert = new AlertDialog.Builder(PerfilFragment.this.getContext());
-            alert.setMessage("¿Deseas cerrar sesion?")
-                    .setCancelable(false)
-                    .setPositiveButton("Si", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            fAuth.signOut();
-                            startActivity(new Intent(PerfilFragment.this.getContext(), login.class));
-                        }
-                    })
-                    .setNegativeButton("No", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            dialog.cancel();
-                        }
-                    });
-            AlertDialog title = alert.create();
-            title.setTitle("Cerrar Sesion");
-            title.show();
+
+        buttonsalir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDialogcerrarsesion();
+            }
         });
+
+
+
         if (fAuth.getCurrentUser() == null) {
             startActivity(new Intent(getContext(), login.class));
             onStop();
@@ -111,20 +101,18 @@ public class PerfilFragment extends Fragment {
         resetPassLocal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                final EditText resetPassword = new EditText(v.getContext());
-
-                final AlertDialog.Builder passwordResetDialog = new AlertDialog.Builder(v.getContext());
-                passwordResetDialog.setTitle("Resetiar Contraseña ?");
-                passwordResetDialog.setMessage("Ingresa Nueva Contraseña > 6 Caracteres Minimo.");
-                passwordResetDialog.setView(resetPassword);
+                resetPassword.findViewById(R.id.editcontra);
+                dialog.setContentView(R.layout.resetcontra);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                sireset = dialog.findViewById(R.id.btn_yesreset);
+                noreset = dialog.findViewById(R.id.btn_noreset);
 
 
-                passwordResetDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                sireset.setOnClickListener(new View.OnClickListener() {
                     @Override
-                    public void onClick(DialogInterface dialog, int which) {
+                    public void onClick(View v ) {
                         // extract the email and send reset link
-                        String newPassword = resetPassword.getText().toString();
+                        newPassword = resetPassword.getText().toString();
                         user.updatePassword(newPassword).addOnSuccessListener(new OnSuccessListener<Void>() {
                             @Override
                             public void onSuccess(Void aVoid) {
@@ -139,14 +127,14 @@ public class PerfilFragment extends Fragment {
                     }
                 });
 
-                passwordResetDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                noreset.setOnClickListener(new View.OnClickListener() {
                     @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        // close
+                    public void onClick(View v) {
+                        dialog.cancel();
                     }
                 });
 
-                passwordResetDialog.create().show();
+                dialog.show();
 
             }
         });
@@ -154,19 +142,115 @@ public class PerfilFragment extends Fragment {
         changeProfileImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // open gallery
-                Intent i = new Intent(v.getContext(), EditProfile.class);
-                i.putExtra("fullName", fullName.getText().toString());
-                i.putExtra("email", email.getText().toString());
-                i.putExtra("phone", phone.getText().toString());
-                startActivity(i);
-//
-
+                openDialog();
+                Nombre = dialog.findViewById(R.id.Name);
+                Email = dialog.findViewById(R.id.Email);
+                Phone = dialog.findViewById(R.id.Phone);
+                fStore.collection("users").document(userId).addSnapshotListener(new EventListener<DocumentSnapshot>() {
+                    @Override
+                    public void onEvent(@androidx.annotation.Nullable DocumentSnapshot value, @androidx.annotation.Nullable FirebaseFirestoreException error) {
+                        if(value.exists()){
+                            Nombre.setText(value.getString("fName"));
+                            Email.setText(value.getString("email"));
+                            Phone.setText(value.getString("phone"));
+                        }
+                    }
+                });
             }
         });
 
 
         return root;
+    }
+
+    private void openDialogcerrarsesion() {
+        dialog.setContentView(R.layout.cerrarseion);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        //dialog.setCancelable(false);
+        Button sicerrar = dialog.findViewById(R.id.btn_yescerrar);
+        Button nocerrar = dialog.findViewById(R.id.btn_nocerrar);
+
+        sicerrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fAuth.signOut();
+                startActivity(new Intent(PerfilFragment.this.getContext(), login.class));
+            }
+        });
+
+        nocerrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.cancel();
+            }
+        });
+        dialog.show();
+
+
+    }
+
+    private void openDialog() {
+        dialog.setContentView(R.layout.cus);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.setCancelable(false);
+        ImageView imageViewclose = dialog.findViewById(R.id.imageclose2);
+        Button btnsave = dialog.findViewById(R.id.btn_yes);
+        Button btnno = dialog.findViewById(R.id.btn_no);
+        //EditText Nombre = dialog.findViewById(R.id.Name);
+
+
+        imageViewclose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+                Toast.makeText(PerfilFragment.this.getContext(), "Cerrado", Toast.LENGTH_SHORT).show();
+            }
+        });
+        btnsave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(Nombre.getText().toString().isEmpty() || Email.getText().toString().isEmpty() || Phone.getText().toString().isEmpty()){
+                    Toast.makeText(PerfilFragment.this.getContext(), "Uno o varios campos estan vacios.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                final String email = Email.getText().toString();
+                user.updateEmail(email).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        DocumentReference docRef = fStore.collection("users").document(user.getUid());
+                        Map<String,Object> edited = new HashMap<>();
+                        edited.put("email",email);
+                        edited.put("fName",Nombre.getText().toString());
+                        edited.put("phone",Phone.getText().toString());
+                        docRef.update(edited).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Toast.makeText(PerfilFragment.this.getContext(), "Perfil actualizado", Toast.LENGTH_SHORT).show();
+                                //onBackPressed();
+                                dialog.dismiss();
+                                return;
+                            }
+                        });
+                        //Toast.makeText(EditProfile.this, "Perfil cambiado.", Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(PerfilFragment.this.getContext(),   e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+        btnno.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.cancel();
+                Toast.makeText(PerfilFragment.this.getContext(), "Cancelado", Toast.LENGTH_SHORT).show();
+            }
+        });
+        dialog.show();
+
     }
 
     @Override
@@ -178,10 +262,3 @@ public class PerfilFragment extends Fragment {
         }//sacar usuario
     }
 }
-
-
-
-
-
-
-
